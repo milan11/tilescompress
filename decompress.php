f8e491e9de3b2f04e94c1c7ee8218fc0acfb89ba
Milan Laslop <milan33@gmail.com>
2021-02-12 20:37:39 +0100

    fix - decompress.php should include decompressing.php

--- decompress.php
+++ decompress.php
@@ -1,7 +1,7 @@
 <?php
 
 require_once 'internal/error_handling.php';
-require_once 'internal/compressing.php';
+require_once 'internal/decompressing.php';
 require_once 'internal/TileResponseHandler_ToFile.php';
 
 decompressOne($argv[1], (int) $argv[2], (int) $argv[3], (int) $argv[4], new TileResponseHandler_ToFile($argv[5]));
